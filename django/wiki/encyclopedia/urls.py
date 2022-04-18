6c689d665393ad0636da897e085ba82c99be449f
technodude10 <technodudetrickster@gmail.com>
2022-04-18 22:06:40 +0530

    newpage

--- django/wiki/encyclopedia/urls.py
+++ django/wiki/encyclopedia/urls.py
@@ -6,5 +6,6 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.wiki, name="wiki"),
-    path("search", views.search, name="search")
+    path("search", views.search, name="search"),
+    path("newpage", views.newpage, name="newpage")
 ]
--- django/wiki/encyclopedia/views.py
+++ django/wiki/encyclopedia/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django import forms
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-
+from django.http import HttpResponse
+from markdown2 import Markdown
 from . import util
+
 
 class NewForm(forms.Form):
     forms = forms.CharField(label='', 
                     widget=forms.TextInput(attrs={'placeholder': 'Search Encyclopedia'}))
 
+markdowner = Markdown()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -18,30 +19,47 @@ def index(request):
 
 def wiki(request, title):
     if not util.get_entry(title):
-        return render(request, "encyclopedia/error.html")
+        return render(request, "encyclopedia/error.html", {
+                "message": "Content not available"
+            })
     else:
-        data = util.get_entry(title)
+        md = util.get_entry(title)
+        data = markdowner.convert(md)
         return render(request, "encyclopedia/entry.html", {
-            "data": data, "form": NewForm
+            "data": data, "form": NewForm, "title": title
         })
 
 def search(request):
     if request.method == "POST":
         form = NewForm(request.POST)
         if form.is_valid():
-            forms = form.cleaned_data["forms"]
-            
-    if util.get_entry(forms):
-        data = util.get_entry(forms)
-        return render(request, "encyclopedia/entry.html", {
-            "data": data, "form": NewForm
-        })
+            title = form.cleaned_data["forms"]
+
+    
+    if util.get_entry(title):
+        return redirect(f'wiki/{title}')
+
 
     possible_entry = []
     for entry in util.list_entries():
-        if forms.lower() in entry.lower():
+        if title.lower() in entry.lower():
             possible_entry.append(entry)
     return render(request, "encyclopedia/search.html", {
         "entries": possible_entry, 
         "form": NewForm
     })
+
+def newpage(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        if util.get_entry(title) != None:
+            return render(request, "encyclopedia/error.html", {
+                "message": "Page already exists"
+            })
+        else:
+            util.save_entry(title, content)
+            return redirect(f'wiki/{title}')
+
+    else:
+        return render(request, "encyclopedia/newpage.html")
