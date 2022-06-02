62f33439848289beed59a5f1ef9afce92c106dfb
technodude10 <technodudetrickster@gmail.com>
2022-06-02 16:59:26 +0530

    .

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Listing(models.Model):
+    pass
+
+class Bid(models.Model):
+    pass
+
+class Comments(models.Model):
+    pass
