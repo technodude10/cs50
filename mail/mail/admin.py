43d6fda0b72d7d448a5bb1282581ab9916a27a4f
Annlee Fores <annleefores@gmail.com>
2022-06-08 16:56:55 +0530

    .

--- mail/mail/admin.py
+++ mail/mail/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Email
+# Register your models here.
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Email)
--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -20,7 +20,30 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
-}
+
+    document.querySelector('#compose-form').onsubmit = () => {
+      const recipients = document.querySelector('#compose-recipients').value;
+      const subject = document.querySelector('#compose-subject').value;
+      const body = document.querySelector('#compose-body').value;
+
+      fetch('/emails', {
+        method: 'POST',
+        body: JSON.stringify({
+            recipients: recipients,
+            subject: subject,
+            body: body
+        })
+      })
+      .then(response => response.json())
+      .then(result => {
+          // Print result
+          console.log(result);
+      });
+      load_mailbox('sent');
+      return false;
+  };
+
+};
 
 function load_mailbox(mailbox) {
   
@@ -30,4 +53,13 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-}
+
+    fetch(`/emails/${mailbox}`)
+    .then(response => response.json())
+    .then(emails => {
+        // Print emails
+        console.log(emails);
+
+        // ... do something else with emails ...
+    });
+};
