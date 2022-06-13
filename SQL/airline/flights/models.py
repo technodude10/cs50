437a654e853a3796ed0d4506a81e277243b0a442
Annlee Fores <annleefores@gmail.com>
2022-06-13 16:48:33 +0530

    .

--- SQL/airline/flights/models.py
+++ SQL/airline/flights/models.py
@@ -13,10 +13,11 @@ class Flight(models.Model):
     destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
     duration = models.IntegerField()
 
-
-
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
+
+    def is_valid_flight(self):
+        return self.origin != self.destination and self.duration > 0
 
 
 class Passenger(models.Model):
