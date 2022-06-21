02d5c2b0c4380d8be1c7ad5cf9446d9b8bd0f8ef
Annlee Fores <annleefores@gmail.com>
2022-06-21 11:23:59 +0530

    .

--- reactjstut/mynotes/src/pages/NotesListPage.js
+++ reactjstut/mynotes/src/pages/NotesListPage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import notes from '../assets/data'
+import Listitem from '../components/Listitem'
 
 const NotesListPage = () => {
   return (
     <div>
         <div className='notes-list'>
-            {notes.map(note => (
-                <p>{note.body}</p>
+            {notes.map((note, index) => (
+                <Listitem key={index} note={note} />
             ))}
         </div>
     </div>
