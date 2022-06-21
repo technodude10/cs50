0f42d5c37d0cdf261b2fc8a12b28112c5db33eba
Annlee Fores <annleefores@gmail.com>
2022-06-21 16:56:11 +0530

    .

--- reactjstut/mynotes/src/pages/NotePage.js
+++ reactjstut/mynotes/src/pages/NotePage.js
@@ -1,24 +1,95 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
-import notes from '../assets/data'
-import { Link } from 'react-router-dom'
+import React, {useState, useEffect} from 'react'
+import { useParams, useNavigate } from 'react-router-dom';
+// import notes from '../assets/data'
 import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg'
 
 const NotePage = () => {
 
-  let noteID = useParams();
-  let note = notes.find(note => note.id === Number(noteID.id))
+  let note_id = useParams();
+  let noteID = note_id.id;
+ 
+  let [note, setNote] = useState([])
+
+
+  useEffect(() => {
+    getNote()
+  }, [noteID])
+
+
+  let getNote = async () => {
+    if (noteID === 'new') return
+
+    let response = await fetch(`http://localhost:8000/notes/${noteID}`)
+    let data = await response.json()
+    setNote(data)
+  }  
+
+  let updateNote = async () => {
+    await fetch(`http://localhost:8000/notes/${noteID}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({...note, 'updated': new Date()})
+    })
+  }
+
+  let createNote = async () => {
+    await fetch(`http://localhost:8000/notes/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({...note, 'updated': new Date()})
+    })
+  }
+
+  const navigate = useNavigate();
+
+  let deleteNote = async () => {
+    await fetch(`http://localhost:8000/notes/${noteID}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    navigate(-1)
+  }
+
+  let handleSubmit = () => {
+
+    if (noteID !== 'new' && !note.body) {
+      deleteNote()
+    } else if (noteID !== 'new') {
+      updateNote()
+      navigate(-1)
+    } else if (noteID === 'new' && note !== null) {
+      createNote()
+      navigate(-1)
+    }
+
+    
+  }
 
   return (
+    
     <div className='note'>
       <div className='note-header'>
+
         <h3>
-            <Link to="/" >
-                <ArrowLeft/>
-            </Link>
+            <ArrowLeft onClick={handleSubmit}/>
         </h3>
+
+
+        {noteID !== 'new' ? (
+          <button onClick={deleteNote}>Delete</button>
+        ) : (
+          <button onClick={handleSubmit}>Done</button>
+        )}
+
+
       </div>
-      <textarea value={note.body}>
+      <textarea onChange={(e)=> {setNote({...note, 'body':e.target.value})}} value={note.body}>
 
       </textarea>
     </div>
--- reactjstut/mynotes/src/pages/NotesListPage.js
+++ reactjstut/mynotes/src/pages/NotesListPage.js
@@ -1,8 +1,24 @@
-import React from 'react'
-import notes from '../assets/data'
+import React, {useState, useEffect} from 'react'
+// import notes from '../assets/data'
 import Listitem from '../components/Listitem'
+import AddButton from '../components/AddButton'
+
 
 const NotesListPage = () => {
+
+  let [notes, setNotes] = useState([])
+
+  useEffect(() => {
+    getNotes()
+  }, [])
+
+  let getNotes = async () => {
+    let response = await fetch('http://localhost:8000/notes/')
+    let data = await response.json()
+    setNotes(data)
+  }
+  
+
   return (
     <div className='notes'>
       <div className='notes-header'>
@@ -14,6 +30,7 @@ const NotesListPage = () => {
                 <Listitem key={index} note={note} />
             ))}
         </div>
+        <AddButton />
     </div>
   )
 }
