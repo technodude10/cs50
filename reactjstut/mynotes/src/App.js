06c42d509622afe1c4f5417c71b395abe180324c
Annlee Fores <annleefores@gmail.com>
2022-06-21 11:56:32 +0530

    .

--- reactjstut/mynotes/src/App.js
+++ reactjstut/mynotes/src/App.js
@@ -1,13 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import './App.css';
 import Header from './components/Header';
 import NotesListPage from './pages/NotesListPage';
+// import NotePage from './pages/NotePage';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <Header />
-      <NotesListPage />
+      <Routes>
+          <Route path="/" element={<NotesListPage />} />
+          {/* <Route path="note" element={<NotePage />} /> */}
+      </Routes>
     </div>
+    </Router>
   );
 }
 
